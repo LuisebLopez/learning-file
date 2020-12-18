db23868075889ceb220c498fad0b18742a3c34dc
Luis Lopez <luise.lopez@unitec.edu>
2020-12-18 01:39:00 -0600

    primer save

---
+++ app.js
@@ -0,0 +1,8 @@
+const EventsClass = require("events");
+const events = new EventsClass();
+
+events.on("logging", (arg) => {
+  console.log(`mensaje aqui: ${JSON.stringify(arg, null, 2)}`);
+});
+
+events.emit("logging", { id: 1, data: "boludo klk" });
